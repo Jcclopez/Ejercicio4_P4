3e41d2fb9ec52f401c5526d4222d2e74a90ab5e9
Jcclopez <pruebasjcc1923@gmail.com>
2022-01-15 17:21:54 +0100

    Agrego una linea en pruebas

--- PUD02EJ08.java
+++ PUD02EJ08.java
@@ -9,6 +9,7 @@ public class PUD02EJ08 {
 				multa=(vel>=0 && vel<60)?300:(vel>=61 && vel<=120)?0:(vel>=121 && vel<=140)?300:(vel>=141 && vel<=180)?600:(vel>180)?1000:0;
 				//DECLARAMOS EN MULTA Y HACEMOS LAS OPERACIONES TERNARIAS PARA COMPROBAR QUE VELOCIDAD ES LA ADECUADA
 				System.out.println("Multa : "+multa+" €");
+				System.out.println("AÃ±ado linea pruebas");
 		teclado.close();
 	}
 
